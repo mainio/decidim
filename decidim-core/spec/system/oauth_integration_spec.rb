234de58e5f91b9ad781e840cfb79ec5ebbc92d62
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2025-07-30 10:51:08 +0300

    Fix some flaky specs in `decidim-core`

--- decidim-core/spec/system/oauth_integration_spec.rb
+++ decidim-core/spec/system/oauth_integration_spec.rb
@@ -11,6 +11,7 @@ describe "Authorzing with OAUth applications", type: :system do
     switch_to_host(organization.host)
     login_as user, scope: :user
     visit decidim.oauth_authorization_url(host: application.owner.host, client_id: application.uid, redirect_uri: application.redirect_uri, response_type: "code")
+    expect(page).to have_content("wants to use your account")
   end
 
   describe "authorization screen" do
@@ -25,6 +26,7 @@ describe "Authorzing with OAUth applications", type: :system do
   describe "authorize the application" do
     it "redirects to the redirect uri with a code" do
       click_button "Authorize application"
+      expect(page).to have_content("DNS_PROBE_FINISHED_NXDOMAIN")
 
       expect(current_url).to start_with("#{application.redirect_uri}?code=")
     end
@@ -33,6 +35,7 @@ describe "Authorzing with OAUth applications", type: :system do
   describe "cancel the request" do
     it "redirects to the redirect uri with an error" do
       click_button "Cancel"
+      expect(page).to have_content("DNS_PROBE_FINISHED_NXDOMAIN")
 
       expect(current_url).to start_with("#{application.redirect_uri}?error=access_denied&error_description=")
     end
