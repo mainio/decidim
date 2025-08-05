7674549a8515f9ee817d12cb191f997f6ea06b63
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2025-08-05 14:20:25 +0300

    Remove unnecessary commented import statements from the `editor.js`

--- decidim-core/app/packs/src/decidim/editor.js
+++ decidim-core/app/packs/src/decidim/editor.js
@@ -2,11 +2,6 @@
 
 import Quill, { createServerUploader } from "dippen";
 
-// import lineBreakButtonHandler from "src/decidim/editor/linebreak_module";
-// import "src/decidim/editor/clipboard_override";
-// import "src/decidim/vendor/image-resize.min";
-// import "src/decidim/vendor/image-upload.min";
-
 const quillFormats = [
   "bold",
   "italic",
